6c7bb1d09f3ba6933ef991b61fea0c6d091b488f
BaLe2003 <77970250+BaLe2003@users.noreply.github.com>
2022-01-10 15:11:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Ba yeu Thanh",
+    introTitle: 'Heluu bé iu!',
+    introDesc: `Anh có điều này muốn hỏi bé nhớ phải trả lời thật lòng nhaaa:3333`,
+    btnIntro: 'Dạ!',
+    title: 'Bé iu anh có phải không nào ._.',
+    desc: 'Nếu bé ko trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v ',
+    btnYes: 'Yêu ơi là yêu <3',
+    btnNo: 'Anh mơ à ???',
+    question: 'Lí do bé thích anh đi :vvv',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì anh đẹp try và dethuong vlll lun asaaa ♥, bé iu anh lắm lắmmmm',
+    mess: 'Anh biết mà ^^ Yêu bé x1000.000',
+    messDesc: 'Tối nay anh qua đón bé đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho anh đi nàooo',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: '' 
+}
